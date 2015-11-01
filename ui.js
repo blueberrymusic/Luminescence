4e12d038f4ca8b22a6a09b24fabadce33b1442ce
Andrew Glassner <andrew@imaginary-institute.com>
2015-10-31 21:21:31 -0700

    first commit. load/save panel probably not set up right

---
+++ ui.js
@@ -0,0 +1,18 @@
+"use strict";
+
+function helpButtonFunction() {
+	alert("help button");
+	//window.open("http://www.glassner.com");
+}
+function saveButtonFunction() {
+	alert("save Button");
+}
+
+function loadButtonFunction() {
+	alert("load Button");
+}
+
+function deleteButtonFunction() {
+	alert("delete Button");
+}
+
